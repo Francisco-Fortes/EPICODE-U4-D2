51a4670bb13b88c5044ea0d8605d3cabbb38b941
Francisco-Fortes <fortesfco@gmail.com>
2022-12-21 22:42:07 +0100

    test

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import authorsRouter from "./api/authors/index.js";
 import listEndpoints from "express-list-endpoints";
 //after adding "type": "module" to package.json
-const server = express();
+const server = express(); //needs to parse the Body
 const port = 3001; //it cannot be changed
 //node ./src/server.js to run the server
 
